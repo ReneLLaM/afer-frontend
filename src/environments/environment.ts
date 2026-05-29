c9575d603e2939fc2712953ddb805406c32a7e3f
Rene <renellanosmachuca@gmail.com>
2026-05-29 18:14:39 -0400

    modo desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: 'https://api.aferbolivia.com/api',
-  // baseUrl: 'http://localhost:3000/api',
-};
+  // baseUrl: 'https://api.aferbolivia.com/api',
+  baseUrl: 'http://localhost:3000/api',
+}; 
